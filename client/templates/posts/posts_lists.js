4af3e5c4de0bfcb865d17bc2ea8b0eb0dd77e390
Jaakko Piipponen <jaakko.piipponen@gmail.com>
2015-08-20 09:28:52 -0500

    finish commenting, add notifications with security rules

--- client/templates/posts/posts_lists.js
+++ client/templates/posts/posts_lists.js
@@ -3,17 +3,3 @@ Template.postsList.helpers({
 		return Posts.find({}, {sort: {submitted:-1}});
 	}
 });
-
-Template.postItem.helpers({
-	ownPost: function() {
-		return this.userId === Meteor.userId();
-	},
-	domain: function() {
-		var a = document.createElement('a');
-		a.href = this.url;
-		return a.hostname;
-	},
-	commentsCount: function() {
-		return Comments.find({postId: this._id}).count();
-	}
-});
