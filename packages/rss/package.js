6f861b342de610d023989f79fe008b2d999de192
Jaakko Piipponen <jaakko.piipponen@gmail.com>
2015-08-21 07:09:34 -0500

    add rss and API

---
+++ packages/rss/package.js
@@ -0,0 +1,27 @@
+Package.describe({
+  name: 'jaakko:rss',
+  version: '0.0.1',
+  // Brief, one-line summary of the package.
+  summary: 'RSS Feed generator',
+  // URL to the Git repository containing the source code for this package.
+  git: '',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md'
+});
+
+Npm.depends({rss: '1.1.1'});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.1.0.3');
+  api.addFiles('rss.js', 'server');
+  api.export('RSS');
+});
+
+/*
+Package.onTest(function(api) {
+  api.use('tinytest');
+  api.use('jaakko:rss');
+  api.addFiles('rss-tests.js');
+});
+*/
